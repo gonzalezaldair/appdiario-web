bd06e82a7af89f0365205e3e8e3d889a70170258
Aldair Gonzalez <aldairgonzalez940121@gmail.com>
2020-02-17 21:46:29 -0500

    se inicio el proyecto, interfaz grafica

---
+++ vistas/modulos/nav/lateral.php
@@ -0,0 +1,109 @@
+<!-- Main Sidebar Container -->
+<aside class="main-sidebar sidebar-dark-primary elevation-4">
+  <!-- Brand Logo -->
+  <a href="index3.html" class="brand-link">
+    <img src="vistas/dist/img/AdminLTELogo.png" alt="AdminLTE Logo" class="brand-image img-circle elevation-3"
+         style="opacity: .8">
+    <span class="brand-text font-weight-light">AdminLTE 3</span>
+  </a>
+
+  <!-- Sidebar -->
+  <div class="sidebar">
+    <!-- Sidebar user panel (optional) -->
+    <div class="user-panel mt-3 pb-3 mb-3 d-flex">
+      <div class="image">
+        <img src="vistas/dist/img/user2-160x160.jpg" class="img-circle elevation-2" alt="User Image">
+      </div>
+      <div class="info">
+        <a href="#" class="d-block">Alexander Pierce</a>
+      </div>
+    </div>
+
+    <!-- Sidebar Menu -->
+    <nav class="mt-2">
+      <ul class="nav nav-pills nav-sidebar flex-column" data-widget="treeview" role="menu" data-accordion="false">
+        <!-- Add icons to the links using the .nav-icon class
+             with font-awesome or any other icon font library -->
+        <li class="nav-item has-treeview menu-open">
+          <a href="#" class="nav-link active">
+            <i class="nav-icon fas fa-tachometer-alt"></i>
+            <p>
+              Prestamos
+              <i class="right fas fa-angle-left"></i>
+            </p>
+          </a>
+          <ul class="nav nav-treeview">
+            <li class="nav-item">
+              <a href="prestamos" class="nav-link active">
+                <i class="far fa-circle nav-icon"></i>
+                <p>Prestamos</p>
+              </a>
+            </li>
+            <li class="nav-item">
+              <a href="abonos" class="nav-link">
+                <i class="far fa-circle nav-icon"></i>
+                <p>Abono</p>
+              </a>
+            </li>
+          </ul>
+        </li>
+        <li class="nav-item">
+          <a href="forma-pago" class="nav-link">
+            <i class="nav-icon fas fa-th"></i>
+            <p>
+              Forma de Pago
+              <!--<span class="right badge badge-danger">New</span>-->
+            </p>
+          </a>
+        </li>
+        <li class="nav-item">
+          <a href="ruta" class="nav-link">
+            <i class="nav-icon fas fa-th"></i>
+            <p>
+              Ruta
+              <!--<span class="right badge badge-danger">New</span>-->
+            </p>
+          </a>
+        </li>
+        <li class="nav-item">
+          <a href="clientes" class="nav-link">
+            <i class="nav-icon fas fa-th"></i>
+            <p>
+              Clientes
+              <!--<span class="right badge badge-danger">New</span>-->
+            </p>
+          </a>
+        </li>
+        <li class="nav-item">
+          <a href="cobro" class="nav-link">
+            <i class="nav-icon fas fa-th"></i>
+            <p>
+              Cobro
+              <!--<span class="right badge badge-danger">New</span>-->
+            </p>
+          </a>
+        </li>
+        <li class="nav-item">
+          <a href="rol" class="nav-link">
+            <i class="nav-icon fas fa-th"></i>
+            <p>
+              Rol
+              <!--<span class="right badge badge-danger">New</span>-->
+            </p>
+          </a>
+        </li>
+        <li class="nav-item">
+          <a href="usuarios" class="nav-link">
+            <i class="nav-icon fas fa-th"></i>
+            <p>
+              Usuario
+              <!--<span class="right badge badge-danger">New</span>-->
+            </p>
+          </a>
+        </li>
+      </ul>
+    </nav>
+    <!-- /.sidebar-menu -->
+  </div>
+  <!-- /.sidebar -->
+</aside>
---
+++ vistas/modulos/navegador.php
@@ -0,0 +1,5 @@
+<?php
+
+	include 'nav/cabecera.php';
+
+	include 'nav/lateral.php';
