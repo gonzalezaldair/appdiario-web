7cafd40c1bd3511cafada331bee0c8616841845c
Aldair Gonzalez <Aldairgonzalez940121@gmail.com>
2020-02-27 20:34:21 -0500

    se agregaron artefactos de esturctura de la bd y los modlaes de agregar segun el modulo

--- vistas/modulos/cobro.vista.php
+++ vistas/modulos/cobro.vista.php
@@ -8,7 +8,7 @@
       <div class="col-sm-6">
         <ol class="breadcrumb float-sm-right">
           <li class="breadcrumb-item"><a href="inicio">Inicio</a></li>
-          <li class="breadcrumb-item active">Cobtro</li>
+          <li class="breadcrumb-item active">Cobro</li>
         </ol>
       </div><!-- /.col -->
     </div><!-- /.row -->
@@ -23,7 +23,7 @@
       <div class="col-12">
         <div class="card">
             <div class="card-header">
-              <button type="button" class="btn btn-primary btn-sm"><i class="fas fa-plus"></i> Nuevo Cobro</button>
+              <button type="button" class="btn btn-primary btn-sm" data-toggle="modal" data-target="#modal-nuevo-cobro"><i class="fas fa-plus"></i> Nuevo Cobro</button>
             </div>
             <!-- /.card-header -->
             <div class="card-body">
@@ -466,3 +466,39 @@
   </div><!-- /.container-fluid -->
 </div>
 <!-- /.content -->
+
+
+<div class="modal fade" id="modal-nuevo-cobro">
+  <div class="modal-dialog">
+    <div class="modal-content">
+      <div class="modal-header">
+        <h4 class="modal-title">Nuevo Cobro</h4>
+        <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+          <span aria-hidden="true">&times;</span>
+        </button>
+      </div>
+      <div class="modal-body">
+        <div class="input-group mb-3">
+          <div class="input-group-prepend">
+            <span class="input-group-text"><i class="fas fa-envelope"></i></span>
+          </div>
+          <input type="text" class="form-control form-control-lg" placeholder="Ingresar Codigo" readonly>
+        </div>
+        <div class="input-group mb-3">
+          <div class="input-group-prepend">
+            <span class="input-group-text"><i class="fas fa-envelope"></i></span>
+          </div>
+          <input type="text" class="form-control form-control-lg" placeholder="Ingresar Nombre">
+        </div>
+      </div>
+      <div class="modal-footer justify-content-between">
+        <button type="button" class="btn btn-danger" data-dismiss="modal"><i class="fas fa-times"></i> Close</button>
+        <button type="button" class="btn btn-primary">Guardar</button>
+      </div>
+    </div>
+    <!-- /.modal-content -->
+  </div>
+  <!-- /.modal-dialog -->
+</div>
+<!-- /.modal -->
+
--- vistas/modulos/usuarios.vista.php
+++ vistas/modulos/usuarios.vista.php
@@ -23,7 +23,7 @@
       <div class="col-12">
         <div class="card">
             <div class="card-header">
-              <button type="button" class="btn btn-primary btn-sm"><i class="fas fa-plus"></i> Nuevo Usuario</button>
+              <button type="button" class="btn btn-primary btn-sm" data-toggle="modal" data-target="#modal-nuevo-usuario"><i class="fas fa-plus"></i> Nuevo Usuario</button>
             </div>
             <!-- /.card-header -->
             <div class="card-body">
@@ -466,3 +466,93 @@
   </div><!-- /.container-fluid -->
 </div>
 <!-- /.content -->
+
+
+
+<div class="modal fade" id="modal-nuevo-usuario">
+  <div class="modal-dialog">
+    <div class="modal-content">
+      <div class="modal-header">
+        <h4 class="modal-title">Nuevo Usuario</h4>
+        <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+          <span aria-hidden="true">&times;</span>
+        </button>
+      </div>
+      <div class="modal-body">
+        <div class="input-group mb-3">
+          <div class="input-group-prepend">
+            <span class="input-group-text"><i class="fas fa-envelope"></i></span>
+          </div>
+          <input type="text" class="form-control form-control-lg" placeholder="Ingresar Cedula">
+        </div>
+        <div class="input-group mb-3">
+          <div class="input-group-prepend">
+            <span class="input-group-text"><i class="fas fa-envelope"></i></span>
+          </div>
+          <input type="text" class="form-control form-control-lg" placeholder="Ingresar Usuario">
+        </div>
+        <div class="input-group mb-3">
+          <div class="input-group-prepend">
+            <span class="input-group-text"><i class="fas fa-envelope"></i></span>
+          </div>
+          <input type="password" class="form-control form-control-lg" placeholder="Ingresar Contraseña">
+        </div>
+        <div class="input-group mb-3">
+          <div class="input-group-prepend">
+            <span class="input-group-text"><i class="fas fa-envelope"></i></span>
+          </div>
+          <input type="text" class="form-control form-control-lg" placeholder="Ingresar Nombre">
+        </div>
+        <div class="input-group mb-3">
+          <div class="input-group-prepend">
+            <span class="input-group-text"><i class="fas fa-envelope"></i></span>
+          </div>
+          <input type="text" class="form-control form-control-lg" placeholder="Ingresar Celular">
+        </div>
+         <div class="input-group mb-3">
+          <div class="input-group-prepend">
+            <span class="input-group-text"><i class="fas fa-envelope"></i></span>
+          </div>
+          <input type="email" class="form-control form-control-lg" placeholder="Ingresar Correo">
+        </div>
+        <div class="input-group mb-3">
+          <div class="input-group-prepend">
+            <span class="input-group-text"><i class="fas fa-envelope"></i></span>
+          </div>
+          <input type="text" class="form-control form-control-lg" placeholder="Ingresar Direccion">
+        </div>
+        <div class="input-group mb-3">
+          <div class="input-group-prepend">
+            <span class="input-group-text"><i class="fas fa-envelope"></i></span>
+          </div>
+          <select class="form-control form-control-lg">
+            <option>option 1</option>
+            <option>option 2</option>
+            <option>option 3</option>
+            <option>option 4</option>
+            <option>option 5</option>
+          </select>
+        </div>
+        <div class="input-group mb-3">
+          <div class="input-group-prepend">
+            <span class="input-group-text"><i class="fas fa-envelope"></i></span>
+          </div>
+          <select class="form-control form-control-lg">
+            <option>option 1</option>
+            <option>option 2</option>
+            <option>option 3</option>
+            <option>option 4</option>
+            <option>option 5</option>
+          </select>
+        </div>
+      </div>
+      <div class="modal-footer justify-content-between">
+        <button type="button" class="btn btn-danger" data-dismiss="modal"><i class="fas fa-times"></i> Close</button>
+        <button type="button" class="btn btn-primary">Guardar</button>
+      </div>
+    </div>
+    <!-- /.modal-content -->
+  </div>
+  <!-- /.modal-dialog -->
+</div>
+<!-- /.modal -->
