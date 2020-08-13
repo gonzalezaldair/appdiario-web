0c481c2a346b3fe6a2988efa5818d25453ade3e7
Aldair Gonzalez <aldairgonzalez940121@gmail.com>
2020-08-13 15:44:00 -0500

    Validacion de correo y otros campos para no permitir caracteres especiales

--- controladores/prestamos.controlador.php
+++ controladores/prestamos.controlador.php
@@ -38,16 +38,18 @@ class PrestamosControlador{
 		$tabla = "prestamo";
 		if (isset($_POST)) {
 
-			if (preg_match('/^[,\\.\\a-zA-Z0-9ñÑáéíóúÁÉÍÓÚ ]+$/', $_POST["pre_Observaciones"]) && preg_match('/^[0-9]+$/', $_POST["pre_CLIENTE"]) && preg_match('/^[0-9]+$/', $_POST["pre_FormaPago"]) && preg_match('/^[0-9]+$/', $_POST["pre_Interes"]) && preg_match('/^[0-9]+$/', $_POST["pre_MontoPrestado"]) && preg_match('/^[0-9]+$/', $_POST["pre_Cuotas"]))
+			$Observaciones = ($_POST["pre_Observaciones"] != "") ? $_POST["pre_Observaciones"] : "Sin Observaciones" ;
+
+			if (preg_match('/^[,\\.\\a-zA-Z0-9ñÑáéíóúÁÉÍÓÚ ]+$/', $Observaciones) && preg_match('/^[0-9]+$/', $_POST["pre_CLIENTE"]) && preg_match('/^[0-9]+$/', $_POST["pre_FormaPago"]) && preg_match('/^[0-9]+$/', $_POST["pre_Interes"]) && preg_match('/^[0-9.,]+$/', $_POST["pre_MontoPrestado"]) && preg_match('/^[0-9]+$/', $_POST["pre_Cuotas"]))
 			{
 				$pre_Id = intval($_POST["pre_Id"]);
-				$pre_Fecha = date('Y-m-d h:m:s');
+				$pre_Fecha = date('Y-m-d H:i:s');
 				$pre_CLIENTE = intval($_POST["pre_CLIENTE"]);
 				$pre_FormaPago = intval($_POST["pre_FormaPago"]);
 				$pre_Interes = intval($_POST["pre_Interes"]);
 				$pre_MontoPrestado = intval($_POST["pre_MontoPrestado"]);
 				$pre_Cuotas = intval($_POST["pre_Cuotas"]);
-				$pre_Observaciones = $_POST["pre_Observaciones"];
+				$pre_Observaciones = $Observaciones;
 				$pre_USUARIO  = intval($_POST["pre_USUARIO"]);
 				$interes = ($pre_Interes > 9) ? "1.".$pre_Interes : "1.0".$pre_Interes ;
 				$pre_MontoInteres = $pre_MontoPrestado*$interes;
@@ -75,7 +77,8 @@ class PrestamosControlador{
 
 				}
 			}else{
-				return "Revisar Campos Alguno debe contener un caracter no permitido o esta vacio";
+				$arrayName = array('codigo' => 'Revisar Campos Alguno debe contener un caracter no permitido o esta vacio');
+				return $arrayName;
 			}
 		}
 	}
