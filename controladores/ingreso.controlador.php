c5107e5a606f5c1de39fca27f6de4b23d8513074
Aldair Gonzalez <aldairgonzalez940121@gmail.com>
2020-08-13 15:31:56 -0500

    se agrego manejo de sesiones

---
+++ controladores/ingreso.controlador.php
@@ -0,0 +1,48 @@
+<?php
+class ingresoControlador
+{
+	public static function ctrIngresarUsuario()
+	{
+		if(isset($_POST["ingresoUsuario"]))
+		{
+			if(preg_match('/^[a-zA-Z0-9-]+$/', $_POST["ingresoUsuario"]) && preg_match('/^[a-zA-Z0-9-]+$/', $_POST["ingresoPassword"]))
+			{
+				//$encriptar = crypt($_POST["ingresoPassword"], '$2a$07$asxx54ahjppf45sd87a5a4dDDGsystemdev$');
+				$encriptar = password_hash($_POST["ingresoPassword"], PASSWORD_DEFAULT, ['cost' => 10]);
+				$datosControador = array(
+					"usu_Login"=>$_POST["ingresoUsuario"],
+					"usu_Password"=>$encriptar
+				);
+
+				$respuesta = IngresoModelo::mdlingresoUsuario($datosControador, "usuario");
+
+				$usuarioActual = trim($_POST["ingresoUsuario"]);
+
+				if (!isset($respuesta["mensaje"])) {
+
+					if(is_array($respuesta) &&$respuesta["usu_Login"] == $_POST["ingresoUsuario"] && password_verify($_POST["ingresoPassword"], $respuesta["usu_Password"]))
+					{
+
+						session_start();
+						$_SESSION["validar"] = true;
+						$_SESSION["usuario_Login"] = $respuesta["usu_Login"];
+						$_SESSION["usuario_Id"] = $respuesta["usu_Id"];
+						$_SESSION["usuario_Nombre"] = $respuesta["usu_Nombre"];
+						echo '<script> window.location = "inicio"; </script>';
+
+					}else{
+
+						echo '<div class="alert alert-warning alert-dismissible"> <button type="button" class="close" data-dismiss="alert" aria-hidden="true">&times;</button> <h5><i class="icon fas fa-exclamation-triangle"></i> Advertencia!</h5> Usuario y/o Contraseña incorecctos. </div>';
+					}
+				}else{
+
+					echo '<div class="alert alert-danger alert-dismissible"> <button type="button" class="close" data-dismiss="alert" aria-hidden="true">&times;</button> <h5><i class="icon fas fa-exclamation-triangle"></i> Advertencia!</h5> Error: '.$respuesta["codigo"].'</div>';
+				}
+
+			}else{
+
+				echo '<div class="alert alert-danger alert-dismissible"> <button type="button" class="close" data-dismiss="alert" aria-hidden="true">&times;</button> <h5><i class="icon fas fa-exclamation-triangle"></i> Advertencia!</h5> Los campos Estan vacios o ingresaron caracteres no permitidos. </div>';
+			}
+		}
+	}
+}
---
+++ vistas/modulos/ingreso.vista.php
@@ -0,0 +1,73 @@
+<div class="login-page">
+	<div class="login-box">
+		<div class="login-logo">
+			<a href="#"><b>Admin</b>LTE</a>
+		</div>
+		<!-- /.login-logo -->
+		<div class="card">
+			<div class="card-body login-card-body">
+				<p class="login-box-msg">Iniciar Sesion</p>
+
+				<form method="post">
+					<div class="input-group mb-3">
+						<input type="text" name="ingresoUsuario" class="form-control" placeholder="Ingresar Usuario" required>
+						<div class="input-group-append">
+							<div class="input-group-text">
+								<span class="fas fa-user"></span>
+							</div>
+						</div>
+					</div>
+					<div class="input-group mb-3">
+						<input type="password" name="ingresoPassword" class="form-control" placeholder="Ingresar Contraseña" required>
+						<div class="input-group-append">
+							<div class="input-group-text">
+								<span class="fas fa-lock"></span>
+							</div>
+						</div>
+					</div>
+					<div class="row">
+						<div class="col-8">
+							<div class="icheck-primary">
+								<input type="checkbox" id="remember">
+								<label for="remember">
+									Recordarme
+								</label>
+							</div>
+						</div>
+						<!-- /.col -->
+						<div class="col-4">
+							<button type="submit" class="btn btn-primary btn-block">Ingresar</button>
+						</div>
+						<!-- /.col -->
+					</div>
+				</form>
+
+				<!--<div class="social-auth-links text-center mb-3">
+					<p>- OR -</p>
+					<a href="#" class="btn btn-block btn-primary">
+						<i class="fab fa-facebook mr-2"></i> Sign in using Facebook
+					</a>
+					<a href="#" class="btn btn-block btn-danger">
+						<i class="fab fa-google-plus mr-2"></i> Sign in using Google+
+					</a>
+				</div>-->
+				<!-- /.social-auth-links -->
+
+				<!--<p class="mb-1">
+					<a href="forgot-password.html">I forgot my password</a>
+				</p>
+				<p class="mb-0">
+					<a href="register.html" class="text-center">Register a new membership</a>
+				</p>-->
+			</div>
+			<!-- /.login-card-body -->
+		</div>
+
+          <?php
+          $ingreso = new ingresoControlador();
+          $ingreso -> ctrIngresarUsuario();
+
+          ?>
+	</div>
+	<!-- /.login-box -->
+</div>
